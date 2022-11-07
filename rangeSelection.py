d5cebf7e3cf0546de278f7f8f1a3b2a710679729
Connal Grace <114109106+grace-connal@users.noreply.github.com>
2022-11-06 20:25:12 -0500

    Update rangeSelection.py

--- rangeSelection.py
+++ rangeSelection.py
@@ -1,37 +1,78 @@
-import win32api
-import pyautogui
+import customtkinter
+import tkinter
+
 
 areaCorners = [[], []]
-def rangeSelector():
-    startX = -1
-    startY = -1
-    endX = -1
-    endY = -1
-    # wait for click unless the escape key is pressed
-    # When the escape key is pressed the file terminates without doing anything
-    while win32api.GetKeyState(0x1B) >= 0:
-        # This if statement is true if the left mouse button is pressed down
-        while win32api.GetKeyState(0x01) < 0:
-            # Keep checking for escape key
-            if win32api.GetKeyState(0x1B) < 0:
-                return
-            # X and Y Position of the corner
-            # Only change if the x and y haven't been set yet.
-            # Otherwise, the corner would be updated the whole time the mouse was pressed down
-            if startX == -1 & startY == -1:
-                startX = pyautogui.position().x
-                startY = pyautogui.position().y
-            # This if statement is true if the left mouse button is not pressed down
-            if win32api.GetKeyState(0x01) >= 0:
-                # X and Y Position of the corner
-                endX = pyautogui.position().x
-                endY = pyautogui.position().y
-                return [[startX, startY], [endX, endY]]
+root = customtkinter.CTk()
+root.overrideredirect(True)
+root.resizable(True, True)
+root.geometry("400x400")
+
+root.attributes("-alpha", 0.5)
+
+def moveWindow(event):
+    root.geometry("+" + str(event.x_root) + "+" + str(event.y_root))
 
 
-def getResult(result):
-    areaCorners = result
-    print(areaCorners)
+def resize_left(event):
+    old_left = root.winfo_x()
+    root.geometry("+" + str(event.x_root) + "+" + str(root.winfo_y()))
+    root.geometry(str(root.current_width + old_left - event.x_root) + "x" + str(root.current_height))
+    left_side.place(x=0, y=0)
+    right_side.place(x=root.current_width - 10, y=0)
 
 
-print(getResult(rangeSelector()))
+def resize_right(event):
+    root.geometry(str(event.x_root) + "x" + str(root.current_height))
+    right_side.place(x=root.current_width - 10, y=0)
+
+
+def resize_top(event):
+    old_top = root.winfo_y()
+    root.geometry("+" + str(root.winfo_x()) + "+" + str(event.y_root))
+    root.geometry(str(root.current_width) + "x" + str(root.current_height + old_top - event.y_root))
+    top_side.place(x=0, y=0)
+    bottom_side.place(x=0, y=root.current_height - 10)
+
+
+def resize_bottom(event):
+    root.geometry(str(root.current_width) + "x" + str(event.y_root))
+    bottom_side.place(x=0, y=root.current_height - 10)
+
+
+def resultButton():
+    print([[root.winfo_x(), root.winfo_y()], [root.winfo_x() + root.current_width, root.winfo_y() + root.current_height]])
+    root.destroy()
+
+
+def escClose(event):
+    root.destroy()
+
+
+left_side = tkinter.Frame(master=root, width=10, height=root.current_height, cursor="fleur")
+left_side.place(x=0, y=0)
+left_side.bind('<B1-Motion>', resize_left)
+
+right_side = tkinter.Frame(master=root, width=10, height=root.current_height, cursor="fleur")
+right_side.place(x=root.current_width - 10, y=0)
+right_side.bind('<B1-Motion>', resize_right)
+
+top_side = tkinter.Frame(master=root, width=root.current_width, height=10, cursor="fleur")
+top_side.place(x=0, y=0)
+top_side.bind('<B1-Motion>', resize_top)
+
+bottom_side = tkinter.Frame(master=root, width=root.current_width, height=10, cursor="fleur")
+bottom_side.place(x=0, y=root.current_height - 10)
+bottom_side.bind('<B1-Motion>', resize_bottom)
+
+result_button = customtkinter.CTkButton(master=root, width=20, height=20, text="Select Range", command=resultButton)
+result_button.place(x=20, y=0)
+
+
+move_button = tkinter.Frame(master=root, bg="black", width=20, height=20, cursor="fleur")
+move_button.place(x=0, y=0)
+move_button.bind('<B1-Motion>', moveWindow)
+
+root.bind("<Escape>", lambda event: escClose(event))
+
+root.mainloop()
