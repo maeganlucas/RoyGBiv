a8dd570565e426c17aefd7657a766d84a72a3854
Connal Grace <114109106+grace-connal@users.noreply.github.com>
2022-11-18 21:56:33 -0500

    Update rangeSelection.py

--- rangeSelection.py
+++ rangeSelection.py
@@ -15,32 +15,41 @@ def moveWindow(event):
 
 
 def resize_left(event):
-    old_left = root.winfo_x()
-    root.geometry("+" + str(event.x_root) + "+" + str(root.winfo_y()))
-    root.geometry(str(root.current_width + old_left - event.x_root) + "x" + str(root.current_height))
-    left_side.place(x=0, y=0)
-    right_side.place(x=root.current_width - 10, y=0)
+    if root.current_width + root.winfo_x() - event.x_root >= 250:
+        root.geometry(str(root.current_width + root.winfo_x() - event.x_root) + "x" + str(root.current_height) + "+" + str(event.x_root) + "+" + str(root.winfo_y()))
+        left_side.place(x=0, y=0)
+        top_side.configure(width=root.current_width)
+        bottom_side.configure(width=root.current_width)
+        right_side.place(x=root.current_width - 10, y=0)
 
 
 def resize_right(event):
-    root.geometry(str(event.x_root) + "x" + str(root.current_height))
-    right_side.place(x=root.current_width - 10, y=0)
+    if root.current_width - root.winfo_x() + event.x_root >= 250:
+        root.geometry(str(event.x_root - root.winfo_x()) + "x" + str(root.current_height))
+        top_side.configure(width=root.current_width)
+        bottom_side.configure(width=root.current_width)
+        right_side.place(x=root.current_width - 10, y=0)
 
 
 def resize_top(event):
-    old_top = root.winfo_y()
-    root.geometry("+" + str(root.winfo_x()) + "+" + str(event.y_root))
-    root.geometry(str(root.current_width) + "x" + str(root.current_height + old_top - event.y_root))
-    top_side.place(x=0, y=0)
-    bottom_side.place(x=0, y=root.current_height - 10)
+    if root.current_height + root.winfo_y() - event.y_root >= 75:
+        root.geometry(str(root.current_width) + "x" + str(root.current_height + root.winfo_y() - event.y_root) + "+" + str(root.winfo_x()) + "+" + str(event.y_root))
+        top_side.place(x=0, y=0)
+        left_side.configure(height=root.current_height)
+        right_side.configure(height=root.current_height)
+        bottom_side.place(x=0, y=root.current_height - 10)
 
 
 def resize_bottom(event):
-    root.geometry(str(root.current_width) + "x" + str(event.y_root))
-    bottom_side.place(x=0, y=root.current_height - 10)
+    if root.current_height - root.winfo_y() + event.y_root >= 75:
+        root.geometry(str(root.current_width) + "x" + str(event.y_root - root.winfo_y()))
+        left_side.configure(height=root.current_height)
+        right_side.configure(height=root.current_height)
+        bottom_side.place(x=0, y=root.current_height - 10)
 
 
 def resultButton():
+    areaCorners = [[root.winfo_x(), root.winfo_y()], [root.winfo_x() + root.current_width, root.winfo_y() + root.current_height]]
     print([[root.winfo_x(), root.winfo_y()], [root.winfo_x() + root.current_width, root.winfo_y() + root.current_height]])
     root.destroy()
 
