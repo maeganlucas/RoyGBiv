3910e0c8d788a8c909f9d8eb83a519644fa45389
Connal Grace <114109106+grace-connal@users.noreply.github.com>
2022-11-18 22:10:46 -0500

    Update GUI5testing.py

--- GUI5testing.py
+++ GUI5testing.py
@@ -204,7 +204,7 @@ class App(ctk.CTk):
 
         # Range Selection Event
         def rs_button_event():
-            # ------------------------------------- Connols Code -------------------------------------------------------
+            # ------------------------------------- Connal's Code -------------------------------------------------------
             areaCorners = [[], []]
             rs_win = ctk.CTk()
             rs_win.overrideredirect(True)
@@ -217,41 +217,54 @@ class App(ctk.CTk):
                 rs_win.geometry("+" + str(event.x_root) + "+" + str(event.y_root))
 
             def resize_left(event):
-                old_left = rs_win.winfo_x()
-                rs_win.geometry("+" + str(event.x_root) + "+" + str(rs_win.winfo_y()))
-                rs_win.geometry(str(rs_win.current_width + old_left - event.x_root) + "x" + str(rs_win.current_height))
-                left_side.place(x=0, y=0)
-                right_side.place(x=rs_win.current_width - 10, y=0)
+                if rs_win.current_width + rs_win.winfo_x() - event.x_root >= 250:
+                    rs_win.geometry(str(rs_win.current_width + rs_win.winfo_x() - event.x_root) + "x" + str(
+                        rs_win.current_height) + "+" + str(event.x_root) + "+" + str(rs_win.winfo_y()))
+                    left_side.place(x=0, y=0)
+                    top_side.configure(width=rs_win.current_width)
+                    bottom_side.configure(width=rs_win.current_width)
+                    right_side.place(x=rs_win.current_width - 10, y=0)
 
             def resize_right(event):
-                rs_win.geometry(str(event.x_root) + "x" + str(rs_win.current_height))
-                right_side.place(x=rs_win.current_width - 10, y=0)
+                if rs_win.current_width - rs_win.winfo_x() + event.x_root >= 250:
+                    rs_win.geometry(str(event.x_root - rs_win.winfo_x()) + "x" + str(rs_win.current_height))
+                    top_side.configure(width=rs_win.current_width)
+                    bottom_side.configure(width=rs_win.current_width)
+                    right_side.place(x=rs_win.current_width - 10, y=0)
 
             def resize_top(event):
-                old_top = rs_win.winfo_y()
-                rs_win.geometry("+" + str(rs_win.winfo_x()) + "+" + str(event.y_root))
-                rs_win.geometry(str(rs_win.current_width) + "x" + str(rs_win.current_height + old_top - event.y_root))
-                top_side.place(x=0, y=0)
-                bottom_side.place(x=0, y=rs_win.current_height - 10)
+                if rs_win.current_height + rs_win.winfo_y() - event.y_root >= 75:
+                    rs_win.geometry(str(rs_win.current_width) + "x" + str(
+                        rs_win.current_height + rs_win.winfo_y() - event.y_root) + "+" + str(
+                        rs_win.winfo_x()) + "+" + str(event.y_root))
+                    top_side.place(x=0, y=0)
+                    left_side.configure(height=rs_win.current_height)
+                    right_side.configure(height=rs_win.current_height)
+                    bottom_side.place(x=0, y=rs_win.current_height - 10)
 
             def resize_bottom(event):
-                rs_win.geometry(str(rs_win.current_width) + "x" + str(event.y_root))
-                bottom_side.place(x=0, y=rs_win.current_height - 10)
+                if rs_win.current_height - rs_win.winfo_y() + event.y_root >= 75:
+                    rs_win.geometry(str(rs_win.current_width) + "x" + str(event.y_root - rs_win.winfo_y()))
+                    left_side.configure(height=rs_win.current_height)
+                    right_side.configure(height=rs_win.current_height)
+                    bottom_side.place(x=0, y=rs_win.current_height - 10)
 
             def resultButton():
+                areaCorners = [[rs_win.winfo_x(), rs_win.winfo_y()],
+                               [rs_win.winfo_x() + rs_win.current_width, rs_win.winfo_y() + rs_win.current_height]]
                 print([[rs_win.winfo_x(), rs_win.winfo_y()],
                        [rs_win.winfo_x() + rs_win.current_width, rs_win.winfo_y() + rs_win.current_height]])
-
                 rs_win.destroy()
 
             def escClose(event):
-                rs_win.withdraw()
+                rs_win.destroy()
 
             left_side = tkinter.Frame(master=rs_win, width=10, height=rs_win.current_height, cursor="sb_h_double_arrow")
             left_side.place(x=0, y=0)
             left_side.bind('<B1-Motion>', resize_left)
 
-            right_side = tkinter.Frame(master=rs_win, width=10, height=rs_win.current_height, cursor="sb_h_double_arrow")
+            right_side = tkinter.Frame(master=rs_win, width=10, height=rs_win.current_height,
+                                       cursor="sb_h_double_arrow")
             right_side.place(x=rs_win.current_width - 10, y=0)
             right_side.bind('<B1-Motion>', resize_right)
 
@@ -259,11 +272,13 @@ class App(ctk.CTk):
             top_side.place(x=0, y=0)
             top_side.bind('<B1-Motion>', resize_top)
 
-            bottom_side = tkinter.Frame(master=rs_win, width=rs_win.current_width, height=10, cursor="sb_v_double_arrow")
+            bottom_side = tkinter.Frame(master=rs_win, width=rs_win.current_width, height=10,
+                                        cursor="sb_v_double_arrow")
             bottom_side.place(x=0, y=rs_win.current_height - 10)
             bottom_side.bind('<B1-Motion>', resize_bottom)
 
-            result_button = ctk.CTkButton(master=rs_win, width=20, height=20, text="Select Range", command=resultButton)
+            result_button = ctk.CTkButton(master=rs_win, width=20, height=20, text="Select Range",
+                                                    command=resultButton)
             result_button.place(x=20, y=0)
 
             move_button = tkinter.Frame(master=rs_win, bg="black", width=20, height=20, cursor="fleur")
