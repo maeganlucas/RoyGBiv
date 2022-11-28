91e1db4295a66d0169ed01104bfb17932cc0d2d8
Matthew Irvin <63986919+Oisms@users.noreply.github.com>
2022-11-28 15:45:13 -0500

    Added Tritanopia filter capabilities

--- colorcorrection.py
+++ colorcorrection.py
@@ -1,11 +1,8 @@
 import cv2
 import numpy
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
 import PIL
-from PIL import Image
 from PIL import ImageGrab
-import sys
+from colorblind import colorblind
 
 # color correction citation from:
 # https://www.irjet.net/archives/V7/i5/IRJET-V7I5687.pdf
@@ -15,21 +12,26 @@ import sys
 def colorCorrectionDP(protanopia, deutranopia):
     return numpy.array([[1 - deutranopia / 2, deutranopia / 2, 0], [protanopia / 2, 1 - protanopia / 2, 0],  [protanopia / 4, deutranopia / 4, 1 - (protanopia + deutranopia) / 4]]).T
 
-def daltonizationT():
-    return
+# https://ixora.io/projects/colorblindness/color-blindness-simulation-research/used for matrix
+def Tritanopia(img):
+    colorblind_img = colorblind.simulate_colorblindness(img, colorblind_type='t')
+    defection = img - colorblind_img
+    correction = numpy.array([[0, 0, 0], [0.7, 1, 0], [0.7, 0, 1]])
+    Daltonization = numpy.tensordot(defection, correction, axes=([2], [1]))
+    return img + Daltonization
 
 # Connect these values to the slider
 deutranopia = 0.0  # unable to perceive red light
 protanopia = 1.0 # unable to perceive green light
-tritanopia = 0
+tritanopia = 0 # NO CURRENT OPTION FOR A TRITANOPIA SLIDER / MUST BE MUTUALLY EXCLUSIVE FROM DUETRANOPIA AND PROTANOPIA
 
 #imgCorrected = numpy.array(Image.open("example.jpg")) / 255
 imgCorrected = numpy.array(PIL.ImageGrab.grab()) / 255
 transform = colorCorrectionDP(protanopia=protanopia, deutranopia=deutranopia)
-img_corrected = numpy.uint8(numpy.dot(imgCorrected, transform) * 255)
+img_correctedPD = numpy.uint8(numpy.dot(imgCorrected, transform) * 255)
 # Correcting Image for Deutranopia with diagnosed degree of 1.0 and saving the image to file.
-
-
-cv2.imwrite("colorchange.jpg", img_corrected)
+imgDaltonized = Tritanopia(PIL.ImageGrab.grab())
+cv2.imwrite("colorchangePD.jpg", img_correctedPD)
+cv2.imwrite("colorchangeT.jpg", imgDaltonized)
 
 
